20216a1416fb113c8a197c3ad0c77f6ff8b98360
devendarrao049-cmd <devendarrao049@gmail.com>
2026-08-13 18:23:22 +0530

    Add files via upload

---
+++ Practical_1/Selection_sort.py
@@ -0,0 +1,12 @@
+arr = [5, 3, 4, 1, 2]
+
+for i in range(len(arr)):
+    min_index = i
+
+    for j in range(i + 1, len(arr)):
+        if arr[j] < arr[min_index]:
+            min_index = j
+
+    arr[i], arr[min_index] = arr[min_index], arr[i]
+
+print("Sorted array:", arr)
